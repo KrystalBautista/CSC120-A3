21191f51b8b99dc9d170287407ccf12e291e3e4b
KrystalBautista <hbautista@smith.edu>
2025-09-25 21:00:51 -0400

    A3 Complete

--- Conversation.java
+++ Conversation.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
 
 class Conversation implements Chatbot {
 
-  // Attributes 
+  String[] cannedResponses = {
+    "Hmm.."
+  };
 
-  /**
-   * Constructor 
-   */
+  String[] transcript = new String[100];
+  int transcriptIndex = 0;
+
   Conversation() {
     
   }
@@ -14,14 +18,36 @@ class Conversation implements Chatbot {
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("How many conversation rounds?");
+    int rounds = scanner.nextInt();
+    scanner.nextLine();
 
+    String greeting = "Hello! What's on your mind?";
+    System.out.println(greeting);
+    transcript[transcriptIndex++] = greeting;
+
+    for (int i = 0; i < rounds; i++) {
+      String userInput = scanner.nextLine();
+      transcript[transcriptIndex++] = userInput;
+      String response = respond(userInput);
+      System.out.println(response);
+      transcript[transcriptIndex++] = response;
+    }
+
+    String goodbye = "Goodbye!";
+    System.out.println(goodbye);
+    transcript[transcriptIndex++] = goodbye;
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    System.out.println("\nTRANSCRIPT: ");
+    for (int i = 0; i < transcriptIndex; i++) {
+      System.out.println(transcript[i]);
+    }
   }
 
   /**
@@ -30,8 +56,38 @@ class Conversation implements Chatbot {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+    String returnString = inputString; 
+
+
+  /**
+   * Ensures that the replacement doesn't return back to the original input 
+   * (I turning into You but then You turning back into I)
+   */
+  returnString = returnString.replace("I", "__I__");
+  returnString = returnString.replace("me", "__ME__");
+  returnString = returnString.replace("am", "__AM__");
+  returnString = returnString.replace("You", "__YOU__");
+  returnString = returnString.replace("my", "__MY__");
+  returnString = returnString.replace("your", "__YOUR__");
+  returnString = returnString.replace("are", "__ARE__");
+
+  returnString = returnString.replace("__I__", "You");
+  returnString = returnString.replace("__ME__", "you");
+  returnString = returnString.replace("__AM__", "are");
+  returnString = returnString.replace("__YOU__", "I");
+  returnString = returnString.replace("__MY__", "your");
+  returnString = returnString.replace("__YOUR__", "my");
+  returnString = returnString.replace("__ARE__", "am");
+
+    if (!returnString.equals(inputString)) {
+      returnString = returnString + "?";
+    }
+    else {
+      int randomIndex = (int)(Math.random() * cannedResponses.length);
+      returnString = cannedResponses[randomIndex];
+    }
+    
+    return returnString;
   }
 
   public static void main(String[] arguments) {
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+Due to being sick this week I was unable to consult with my classmates as I was not able to attend class.
 - Which session(s) of TA / office hours did you attend?
+I was unable to attend TA / office hours this week.
 - What are your initial impressions of Java? 
+It is more difficult to understand for me compared to Python. It also seems to have a higher standard for what is "correct" as there are things you can get away with in python but java would throw an error.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+Even with coding something in different languages the concept is still very similar. I am able to think more easily in terms of python and then I try and translate that to how it would run in java with an object oriented approach.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I had some trouble at first with the replacing of You, I, etc since there was You -> I but also I->You which would undo the replacement. Using if/else and for loops was very familiar for me and it worked well for this assignment. Advice I would give someone is to be patient with trial and error and don't be afraid to ask for help or look up functions that you don't know but you think would be helpful to use in your code.
